dc796b312bf886a7a2e63283563e7d900ff4dea4
toothlessdev <toothless042@gmail.com>
2024-07-23 23:59:30 +0900

    feat: signin, signup, chat page router 설정

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import { RootLayout } from "./layouts/RootLayout";
+import HomePage from "./pages/home/HomePage";
+import { RootLayout } from "./components/layout/RootLayout";
+import { ChatLayout } from "./components/layout/ChatLayout";
+import ChatPage from "./pages/chat/ChatPage";
+import SignInPage from "./pages/auth/SignInPage";
+import SignUpPage from "./pages/auth/SignUpPage";
 
 export const Router = () => {
     return (
@@ -8,6 +12,12 @@ export const Router = () => {
             <Routes>
                 <Route path="/" element={<RootLayout />}>
                     <Route path="/" element={<HomePage />} />
+                    <Route path="auth/signin" element={<SignInPage />} />
+                    <Route path="auth/signup" element={<SignUpPage />} />
+                </Route>
+
+                <Route path="/chat" element={<ChatLayout />}>
+                    <Route path=":id" element={<ChatPage />}></Route>
                 </Route>
             </Routes>
         </BrowserRouter>

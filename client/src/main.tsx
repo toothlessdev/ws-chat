e0f00bba37b64185d02014866a88083341ed5a7a
toothlessdev <toothless042@gmail.com>
2024-07-23 23:52:31 +0900

    chore: 환경변수 설정

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/globals.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
